435f6b43f9af2e2c4cfc0040c8cb2d5f4b06f17b
Ptyler21 <ptylerjr2@gmail.com>
2019-07-21 22:33:22 -0400

    Roughly 20 min into part 3.

--- Flaskblog.py
+++ Flaskblog.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for
-from flask_script import Manager
+from forms import  RegistrationForm,LoginForm
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'b3012383c08d4cbfedaef20d9561f295'
 posts = [
     {
         'author': 'Phillip Tyler',
@@ -26,6 +27,17 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html',title='About')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html',title='Register',form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Login',form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
