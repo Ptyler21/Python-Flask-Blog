488fe00a22dc5547410fcf0408458ae5b15e18c2
Ptyler21 <ptylerjr2@gmail.com>
2019-07-21 20:18:50 -0400

    the files as of the end of part two in Corey Schafers youtube videos.

--- Flaskblog.py
+++ Flaskblog.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 from flask_script import Manager
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Phillip Tyler',
+        'title':'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'July 21,2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'July 20,2019'
+    }
+
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home page</h1>'
+    return render_template('home.html',posts=posts)
 
 @app.route('/about')
 def about():
-    return '<h1>About page</h1>'
+    return render_template('about.html',title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
